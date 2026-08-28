3e4291775e35acd4d147d5a17ef1b7846a516905
CJ Wanki <136288618+cjwanki@users.noreply.github.com>
2026-08-27 19:55:51 -0600

    Add AWS instance resource for production web app

--- Prod/backend.tf
+++ Prod/backend.tf
@@ -7,3 +7,14 @@ terraform {
     encrypt        = true
   }
 }
+
+resource "aws_instance" "vue_prod_web_1" {
+  ami           = "ami-066a7fbea5161f451"  # Replace with your AMI ID
+  instance_type = "t3.micro"       # Adjust instance type as needed
+  subnet_id              = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.vpc_web_sg.id]
+  
+  tags = {
+    Name = "vue-prod-web-1"
+  }
+}
